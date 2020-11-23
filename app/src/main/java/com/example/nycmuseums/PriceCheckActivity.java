be27faa4bc825cadfd97471e6e48b1d7b94e1dd4
jra165 <36318088+jra165@users.noreply.github.com>
2020-11-23 12:19:49 -0500

    fixed the empty input

--- app/src/main/java/com/example/nycmuseums/PriceCheckActivity.java
+++ app/src/main/java/com/example/nycmuseums/PriceCheckActivity.java
@@ -46,12 +46,11 @@ public class PriceCheckActivity extends AppCompatActivity {
             }
         });
 
+
+
         childQuantity = (EditText) findViewById(R.id.childQuantity);
         adultQuantity = (EditText) findViewById(R.id.adultQuantity);
         seniorQuantity = (EditText) findViewById(R.id.seniorQuantity);
-        childQuantity.setText("0");
-        adultQuantity.setText("0");
-        seniorQuantity.setText("0");
 
 
         calculateButton = (Button) findViewById(R.id.calculate_btn);
@@ -63,45 +62,81 @@ public class PriceCheckActivity extends AppCompatActivity {
             public void onClick(View v) {
 
                 final double TAX_RATE = 0.08875;
-                final double CHILD_RATE = 0;
+                final double CHILD_RATE = 14;
                 final double ADULT_RATE = 25;
                 final double SENIOR_RATE = 18;
+                final int DEFAULT_QUANT = 0;
                 int child_quant;
                 int adult_quant;
                 int senior_quant;
 
 
-                try {
+//                try {
+//
+//                    child_quant = Integer.parseInt(childQuantity.getText().toString());
+//                    adult_quant = Integer.parseInt(adultQuantity.getText().toString());
+//                    senior_quant = Integer.parseInt(seniorQuantity.getText().toString());
+//
+//                    //calculate subtotal
+//                    double subtotal = ((CHILD_RATE * child_quant) + (ADULT_RATE * adult_quant) +
+//                            (SENIOR_RATE * senior_quant));
+//
+//                    //calculate tax
+//                    double total_tax = subtotal * TAX_RATE;
+//
+//                    //calculate final price
+//                    double total_price = subtotal + total_tax;
+//
+//                    // set total price to total TextView
+//                    total.setText(Double.toString(total_price));
+//
+//                }
+//                catch(NumberFormatException ex) {
+//                    //They didn't enter a number.  Pop up a toast or warn them in some other way
+//
+//                    Context context = getBaseContext();
+//                    CharSequence text = "Missing Quantity!";
+//                    int duration = Toast.LENGTH_SHORT;
+//
+//                    Toast toast = Toast.makeText(context, text, duration);
+//                    toast.show();
+//
+//                }
 
+                if (childQuantity.getText().toString().length() > 0) {
                     child_quant = Integer.parseInt(childQuantity.getText().toString());
+                }
+                else {
+                    child_quant = DEFAULT_QUANT;
+                }
+
+                if (adultQuantity.getText().toString().length() > 0) {
                     adult_quant = Integer.parseInt(adultQuantity.getText().toString());
+                }
+                else {
+                    adult_quant = DEFAULT_QUANT;
+                }
+
+                if (seniorQuantity.getText().toString().length() > 0) {
                     senior_quant = Integer.parseInt(seniorQuantity.getText().toString());
-
-                    //calculate subtotal
-                    double subtotal = ((CHILD_RATE * child_quant) + (ADULT_RATE * adult_quant) +
-                            (SENIOR_RATE * senior_quant));
-
-                    //calculate tax
-                    double total_tax = subtotal * TAX_RATE;
-
-                    //calculate final price
-                    double total_price = subtotal + total_tax;
-
-                    // set total price to total TextView
-                    total.setText(Double.toString(total_price));
-
                 }
-                catch(NumberFormatException ex) {
-                    //They didn't enter a number.  Pop up a toast or warn them in some other way
-
-                    Context context = getBaseContext();
-                    CharSequence text = "Missing Quantity!";
-                    int duration = Toast.LENGTH_SHORT;
-
-                    Toast toast = Toast.makeText(context, text, duration);
-                    toast.show();
-
+                else {
+                    senior_quant = DEFAULT_QUANT;
                 }
+
+                //calculate subtotal
+                double subtotal = ((CHILD_RATE * child_quant) + (ADULT_RATE * adult_quant) +
+                        (SENIOR_RATE * senior_quant));
+
+                //calculate tax
+                double total_tax = subtotal * TAX_RATE;
+
+                //calculate final price
+                double total_price = subtotal + total_tax;
+
+                // set total price to total TextView
+                total.setText(Double.toString(total_price));
+
                 //total.setText(String.format("%0.2f", total_price));
             }
         });
