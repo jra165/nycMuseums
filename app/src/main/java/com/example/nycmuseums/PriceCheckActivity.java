19304a2c7bea74d66e41fa7d9aa2dfaea9d24485
jra165 <36318088+jra165@users.noreply.github.com>
2020-11-30 23:50:10 -0500

    finished comments

--- app/src/main/java/com/example/nycmuseums/PriceCheckActivity.java
+++ app/src/main/java/com/example/nycmuseums/PriceCheckActivity.java
@@ -112,15 +112,22 @@ public class PriceCheckActivity extends AppCompatActivity implements AdapterView
 
                 Uri museumSelect = Uri.parse(context.getResources().getString(R.string.moma_link));
 
+                //link to moma
                 if (museumName.equals(context.getResources().getString(R.string.moma_name))) {
                     museumSelect = Uri.parse(context.getResources().getString(R.string.moma_link));
                 }
+
+                //link to met
                 else if (museumName.equals(context.getResources().getString(R.string.met_name))) {
                     museumSelect = Uri.parse(context.getResources().getString(R.string.met_link));
                 }
+
+                //link to guggenheim
                 else if (museumName.equals(context.getResources().getString(R.string.guggenheim_name))) {
                     museumSelect = Uri.parse(context.getResources().getString(R.string.gugg_link));
                 }
+
+                //link to whitney
                 else if (museumName.equals(context.getResources().getString(R.string.whitney_name))) {
                     museumSelect = Uri.parse(context.getResources().getString(R.string.whit_link));
                 }
@@ -194,11 +201,12 @@ public class PriceCheckActivity extends AppCompatActivity implements AdapterView
 }
 
     /**
-     *
-     * @param parent
-     * @param view
-     * @param position
-     * @param id
+     * Interface definition for a callback to be invoked when an item in this view has been selected.
+     * Callback method to be invoked when an item in this view has been selected.
+     * @param parent The AdapterView where the selection happened
+     * @param view The view within the AdapterView that was clicked
+     * @param position The position of the view in the adapter
+     * @param id The row id of the item that is selected
      */
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
@@ -206,8 +214,8 @@ public class PriceCheckActivity extends AppCompatActivity implements AdapterView
     }
 
     /**
-     *
-     * @param parent
+     * Callback method to be invoked when the selection disappears from this view.
+     * @param parent The AdapterView that now contains no selected item.
      */
     @Override
     public void onNothingSelected(AdapterView<?> parent) {
